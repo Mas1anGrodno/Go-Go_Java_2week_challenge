1bce554653be9b7ede865146947ac445b0c4cb49
Kryhin Maksim <masian.grodno@gmail.com>
2024-10-16 15:43:15 +0300

    main file - adding cycle to create objects

--- ecosys/src/main/java/com/mas1an/hopto/Main.java
+++ ecosys/src/main/java/com/mas1an/hopto/Main.java
@@ -1,7 +1,55 @@
 package com.mas1an.hopto;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        // цикл ввода растений
+        while (true) {
+            // Запрос имени для объекта Plant
+            System.out.print("Введите имя для растения: ");
+            String plantName = scanner.nextLine();
+            // Запрос количества для объекта Plant
+            System.out.print("Введите количество растений: ");
+            int plantAmount = scanner.nextInt();
+            // Очистка буфера после ввода числа
+            scanner.nextLine();
+            // Создаем обьект Plant
+            Plant plant = new Plant(plantName, plantAmount);
+            CRUDOperations.createObject(plant);
+
+            System.out.print("Хотите ввести еще одно растение? (Y/N): ");
+            String response = scanner.nextLine();
+            if (response.equalsIgnoreCase("N")) {
+                break;
+            }
+        }
+
+        while (true) {
+            // Запрос имени для объекта Animal
+            System.out.print("Введите имя для животного: ");
+            String animalName = scanner.nextLine();
+            // Запрос количества для объекта Plant
+            System.out.print("Введите количество животных: ");
+            int animalAmount = scanner.nextInt();
+            // Очистка буфера после ввода числа
+            scanner.nextLine();
+            // Создаем обьект Animal
+            Animal animal = new Animal(animalName, animalAmount);
+            CRUDOperations.createObject(animal);
+            System.out.print("Хотите ввести еще одно животное? (Y/N): ");
+            String response = scanner.nextLine();
+            if (response.equalsIgnoreCase("N")) {
+                break;
+            }
+        }
+
+        // Вывод созданных объектов
+        System.out.println("Созданные объекты:");
+        CRUDOperations.readObjects().forEach(System.out::println);
+
+        scanner.close();
     }
 }
